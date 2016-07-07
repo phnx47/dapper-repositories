9bfd51c253d6a7caa05cd07dd346f007f41f847b
Sergey Kuznetsov <sergey.kuznetsov@hotmail.com>
2016-07-07 22:41:14 +0300

    1.4.0-beta2

--- MicroOrm.Dapper.Repositories/DbContext/DapperDbContext.cs
+++ MicroOrm.Dapper.Repositories/DbContext/DapperDbContext.cs
@@ -3,21 +3,34 @@
 namespace MicroOrm.Dapper.Repositories.DbContext
 {
    
-
     /// <summary>
     /// Class is helper for use and close IDbConnection
     /// </summary>
     public class DapperDbContext : IDapperDbContext
     {
+        /// <summary>
+        /// 
+        /// </summary>
         protected DapperDbContext(IDbConnection connection, IDbTransaction transaction = null)
         {
             InnerConnection = connection;
             InnerTransaction = transaction;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
         protected readonly IDbConnection InnerConnection;
+
+        /// <summary>
+        /// 
+        /// </summary>
         protected IDbTransaction InnerTransaction;
 
+
+        /// <summary>
+        /// 
+        /// </summary>
         public virtual IDbConnection Connection
         {
             get
@@ -26,24 +39,38 @@ namespace MicroOrm.Dapper.Repositories.DbContext
                 return InnerConnection;
             }
         }
+
+        /// <summary>
+        /// 
+        /// </summary>
         public virtual IDbTransaction Transaction
         {
             get { return InnerTransaction; }
             set { InnerTransaction = value; }
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
         public virtual void OpenConnection()
         {
             if (InnerConnection.State != ConnectionState.Open && InnerConnection.State != ConnectionState.Connecting)
                 InnerConnection.Open();
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
         public virtual IDbTransaction BeginTransaction()
         {
             OpenConnection();
             InnerTransaction = InnerConnection.BeginTransaction();
             return InnerTransaction;
         }
+
+        /// <summary>
+        /// 
+        /// </summary>
         public void Dispose()
         {
             if (InnerConnection != null && InnerConnection.State != ConnectionState.Closed)
