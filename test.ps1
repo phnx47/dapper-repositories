aca7b1daa72ac6ee8980cb56b5f9b321c4d0bec2
Sergey Kuznetsov <sergey.kuznetsov@hotmail.com>
2018-08-25 18:24:50 +0300

    test.ps1

--- test.ps1
+++ test.ps1
@@ -1,1 +1,1 @@
-OpenCover.Console.exe -register:user -target:"C:/Program Files/dotnet/dotnet.exe" -oldStyle -targetargs:"test ./test/MicroOrm.Dapper.Repositories.Tests/MicroOrm.Dapper.Repositories.Tests.csproj" -filter:"+[MicroOrm*]* -[*.Tests]*" -output:".\coverage.xml"
+OpenCover.Console.exe -register:user -target:"C:/Program Files/dotnet/dotnet.exe" -oldStyle -targetargs:"test ./test/MicroOrm.Dapper.Repositories.Tests/MicroOrm.Dapper.Repositories.Tests.csproj" -filter:"+[MicroOrm*]* -[*.Tests]*" -output:".\coverage.xml" -returntargetcode
