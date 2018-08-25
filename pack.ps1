277125528e92051457f6795d25750e1be8cbd318
Sergey Kuznetsov <sergey.kuznetsov@hotmail.com>
2018-08-25 19:10:03 +0300

    pack.ps1

--- pack.ps1
+++ pack.ps1
@@ -1,7 +1,7 @@
-if($env:APPVEYOR_REPO_TAG -eq "false") {
-    dotnet pack $env:APPVEYOR_BUILD_FOLDER\src\MicroOrm.Dapper.Repositories -c Release --include-symbols --no-build --version-suffix build-$env:APPVEYOR_BUILD_NUMBER -o artifacts\myget
+if ($env:APPVEYOR_REPO_TAG -eq "false") {
+    dotnet pack $env:APPVEYOR_BUILD_FOLDER\src\MicroOrm.Dapper.Repositories -c Release --include-symbols --no-build --version-suffix build$env:APPVEYOR_BUILD_NUMBER -o artifacts\myget
 }
 
-if($env:APPVEYOR_REPO_TAG -eq "true") {
+if ($env:APPVEYOR_REPO_TAG -eq "true") {
     dotnet pack $env:APPVEYOR_BUILD_FOLDER\src\MicroOrm.Dapper.Repositories -c Release --include-symbols --no-build -o artifacts\nuget
 }
