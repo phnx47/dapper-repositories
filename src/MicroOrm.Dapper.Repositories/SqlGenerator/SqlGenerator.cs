55607c7d5dec07ed4a070242e5d9d492be0302fe
Sergey Kuznetsov <cmt@phnx47.net>
2019-04-11 01:08:24 +0300

    Extract AppendWherePredicateQuery()

--- src/MicroOrm.Dapper.Repositories/SqlGenerator/SqlGenerator.cs
+++ src/MicroOrm.Dapper.Repositories/SqlGenerator/SqlGenerator.cs
@@ -364,41 +364,7 @@ namespace MicroOrm.Dapper.Repositories.SqlGenerator
             return query;
         }
 
-        private void AppendWherePredicateQuery(SqlQuery sqlQuery, Expression<Func<TEntity, bool>> predicate, QueryType queryType)
-        {
-            IDictionary<string, object> dictionaryParams = new Dictionary<string, object>();
-
-            if (predicate != null)
-            {
-                // WHERE
-                var queryProperties = new List<QueryExpression>();
-                FillQueryProperties(predicate.Body, ref queryProperties);
-
-                sqlQuery.SqlBuilder.Append("WHERE ");
-
-                var qLevel = 0;
-                var sqlBuilder = new StringBuilder();
-                var conditions = new List<KeyValuePair<string, object>>();
-                BuildQuerySql(queryProperties, ref sqlBuilder, ref conditions, ref qLevel);
-
-                dictionaryParams.AddRange(conditions);
-
-                if (LogicalDelete && queryType == QueryType.Select)
-                    sqlQuery.SqlBuilder.AppendFormat("({3}) AND {0}.{1} != {2} ", TableName, StatusPropertyName, LogicalDeleteValue, sqlBuilder.ToString());
-                else
-                    sqlQuery.SqlBuilder.AppendFormat("{0} ", sqlBuilder.ToString());
-            }
-            else
-            {
-                if (LogicalDelete && queryType == QueryType.Select)
-                    sqlQuery.SqlBuilder.AppendFormat("WHERE {0}.{1} != {2} ", TableName, StatusPropertyName, LogicalDeleteValue);
-            }
-
-            if (LogicalDelete && HasUpdatedAt && queryType == QueryType.Delete)
-                dictionaryParams.Add(UpdatedAtPropertyMetadata.ColumnName, DateTime.UtcNow);
-
-            sqlQuery.SetParam(dictionaryParams);
-        }
+        
 
         /// <summary>
         /// Build the final `query statement and parameters`
