a4e157c9366dff5d3df50e3b5ad8bc9ee784885b
dmxengine <dmxengine@gmail.com>
2019-04-26 18:37:19 +0300

    Fixed MSSQL 2100 parameters limit during BulkInsert and BulkUpdate

--- src/MicroOrm.Dapper.Repositories/DapperRepository.BulkInsert.cs
+++ src/MicroOrm.Dapper.Repositories/DapperRepository.BulkInsert.cs
@@ -1,7 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Data;
+using System.Linq;
 using System.Threading.Tasks;
 using Dapper;
+using MicroOrm.Dapper.Repositories.SqlGenerator;
 
 
 namespace MicroOrm.Dapper.Repositories
@@ -15,17 +18,63 @@ namespace MicroOrm.Dapper.Repositories
         /// <inheritdoc />
         public virtual int BulkInsert(IEnumerable<TEntity> instances, IDbTransaction transaction = null)
         {
+            if (SqlGenerator.Config.SqlProvider == SqlProvider.MSSQL)
+            {
+                int count = 0;
+                int totalInstances = instances.Count();
+
+                var properties =
+                    (SqlGenerator.IsIdentity
+                        ? SqlGenerator.SqlProperties.Where(p => !p.PropertyName.Equals(SqlGenerator.IdentitySqlProperty.PropertyName, System.StringComparison.OrdinalIgnoreCase))
+                        : SqlGenerator.SqlProperties).ToList();
+
+                int exceededTimes = (int)Math.Ceiling(totalInstances * properties.Count / 2100d);
+                if (exceededTimes > 1)
+                {
+                    int maxAllowedInstancesPerBatch = totalInstances / exceededTimes;
+
+                    for (int i = 0; i <= exceededTimes; i++)
+                    {
+                        var items = instances.Skip(i * maxAllowedInstancesPerBatch).Take(maxAllowedInstancesPerBatch);
+                        var msSqlQueryResult = SqlGenerator.GetBulkInsert(items);
+                        count += Connection.Execute(msSqlQueryResult.GetSql(), msSqlQueryResult.Param, transaction);
+                    }
+                    return count;
+                }
+            }
             var queryResult = SqlGenerator.GetBulkInsert(instances);
-            var count = Connection.Execute(queryResult.GetSql(), queryResult.Param, transaction);
-            return count;
+            return Connection.Execute(queryResult.GetSql(), queryResult.Param, transaction);
         }
 
         /// <inheritdoc />
         public virtual async Task<int> BulkInsertAsync(IEnumerable<TEntity> instances, IDbTransaction transaction = null)
         {
+            if (SqlGenerator.Config.SqlProvider == SqlProvider.MSSQL)
+            {
+                int count = 0;
+                int totalInstances = instances.Count();
+
+                var properties =
+                    (SqlGenerator.IsIdentity
+                        ? SqlGenerator.SqlProperties.Where(p => !p.PropertyName.Equals(SqlGenerator.IdentitySqlProperty.PropertyName, System.StringComparison.OrdinalIgnoreCase))
+                        : SqlGenerator.SqlProperties).ToList();
+
+                int exceededTimes = (int)Math.Ceiling(totalInstances * properties.Count / 2100d);
+                if (exceededTimes > 1)
+                {
+                    int maxAllowedInstancesPerBatch = totalInstances / exceededTimes;
+
+                    for (int i = 0; i <= exceededTimes; i++)
+                    {
+                        var items = instances.Skip(i * maxAllowedInstancesPerBatch).Take(maxAllowedInstancesPerBatch);
+                        var msSqlQueryResult = SqlGenerator.GetBulkInsert(items);
+                        count += await Connection.ExecuteAsync(msSqlQueryResult.GetSql(), msSqlQueryResult.Param, transaction);
+                    }
+                    return count;
+                }
+            }
             var queryResult = SqlGenerator.GetBulkInsert(instances);
-            var count = await Connection.ExecuteAsync(queryResult.GetSql(), queryResult.Param, transaction);
-            return count;
+            return await Connection.ExecuteAsync(queryResult.GetSql(), queryResult.Param, transaction);
         }
     }
 }
--- src/MicroOrm.Dapper.Repositories/DapperRepository.BulkUpdate.cs
+++ src/MicroOrm.Dapper.Repositories/DapperRepository.BulkUpdate.cs
@@ -1,7 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Data;
+using System.Linq;
 using System.Threading.Tasks;
 using Dapper;
+using MicroOrm.Dapper.Repositories.SqlGenerator;
 
 namespace MicroOrm.Dapper.Repositories
 {
@@ -20,6 +23,27 @@ namespace MicroOrm.Dapper.Repositories
         /// <inheritdoc />
         public bool BulkUpdate(IEnumerable<TEntity> instances, IDbTransaction transaction)
         {
+            if (SqlGenerator.Config.SqlProvider == SqlProvider.MSSQL)
+            {
+                int count = 0;
+                int totalInstances = instances.Count();
+
+                var properties = SqlGenerator.SqlProperties.ToList();
+
+                int exceededTimes = (int)Math.Ceiling(totalInstances * properties.Count / 2100d);
+                if (exceededTimes > 1)
+                {
+                    int maxAllowedInstancesPerBatch = totalInstances / exceededTimes;
+
+                    for (int i = 0; i <= exceededTimes; i++)
+                    {
+                        var items = instances.Skip(i * maxAllowedInstancesPerBatch).Take(maxAllowedInstancesPerBatch);
+                        var msSqlQueryResult = SqlGenerator.GetBulkUpdate(items);
+                        count += Connection.Execute(msSqlQueryResult.GetSql(), msSqlQueryResult.Param, transaction);
+                    }
+                    return count > 0;
+                }
+            }
             var queryResult = SqlGenerator.GetBulkUpdate(instances);
             var result = Connection.Execute(queryResult.GetSql(), queryResult.Param, transaction) > 0;
             return result;
@@ -34,6 +58,27 @@ namespace MicroOrm.Dapper.Repositories
         /// <inheritdoc />
         public async Task<bool> BulkUpdateAsync(IEnumerable<TEntity> instances, IDbTransaction transaction)
         {
+            if (SqlGenerator.Config.SqlProvider == SqlProvider.MSSQL)
+            {
+                int count = 0;
+                int totalInstances = instances.Count();
+
+                var properties = SqlGenerator.SqlProperties.ToList();
+
+                int exceededTimes = (int)Math.Ceiling(totalInstances * properties.Count / 2100d);
+                if (exceededTimes > 1)
+                {
+                    int maxAllowedInstancesPerBatch = totalInstances / exceededTimes;
+
+                    for (int i = 0; i <= exceededTimes; i++)
+                    {
+                        var items = instances.Skip(i * maxAllowedInstancesPerBatch).Take(maxAllowedInstancesPerBatch);
+                        var msSqlQueryResult = SqlGenerator.GetBulkUpdate(items);
+                        count += await Connection.ExecuteAsync(msSqlQueryResult.GetSql(), msSqlQueryResult.Param, transaction);
+                    }
+                    return count > 0;
+                }
+            }
             var queryResult = SqlGenerator.GetBulkUpdate(instances);
             var result = await Connection.ExecuteAsync(queryResult.GetSql(), queryResult.Param, transaction) > 0;
             return result;
