853ca7813e2ca86b1a1a3a7db747111027938a3b
Serge K <cmt@phnx47.net>
2021-03-12 14:40:28 +0700

    ci: update pack symbols

--- pack.ps1
+++ pack.ps1
@@ -1,7 +1,7 @@
 if ($env:APPVEYOR_REPO_TAG -eq "false") {
-  dotnet pack $env:APPVEYOR_BUILD_FOLDER\src\MicroOrm.Dapper.Repositories -c Release --include-symbols --include-source  --no-build --version-suffix build$env:APPVEYOR_BUILD_NUMBER -o artifacts\myget
+  dotnet pack $env:APPVEYOR_BUILD_FOLDER\src\MicroOrm.Dapper.Repositories -c Release  --no-build --version-suffix build$env:APPVEYOR_BUILD_NUMBER -o artifacts\myget
 }
 
 if ($env:APPVEYOR_REPO_TAG -eq "true") {
-  dotnet pack $env:APPVEYOR_BUILD_FOLDER\src\MicroOrm.Dapper.Repositories -c Release --include-symbols --include-source  --no-build -o artifacts\nuget
+  dotnet pack $env:APPVEYOR_BUILD_FOLDER\src\MicroOrm.Dapper.Repositories -c Release --no-build -o artifacts\nuget
 }
