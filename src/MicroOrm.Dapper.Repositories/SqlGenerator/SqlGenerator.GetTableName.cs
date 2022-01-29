1930ac5e776706f8f23ca4eaf764e08ff3717e3b
Serge K <cmt@phnx47.net>
2022-01-29 15:09:09 +0700

    refactor: extract SqlGenerator.GetTableName

---
+++ src/MicroOrm.Dapper.Repositories/SqlGenerator/SqlGenerator.GetTableName.cs
@@ -0,0 +1,70 @@
+using System;
+using MicroOrm.Dapper.Repositories.Attributes.Joins;
+
+namespace MicroOrm.Dapper.Repositories.SqlGenerator
+{
+    /// <inheritdoc />
+    public partial class SqlGenerator<TEntity>
+        where TEntity : class
+    {
+        private static string GetTableNameWithSchemaPrefix(string tableName, string tableSchema, string startQuotationMark = "", string endQuotationMark = "")
+        {
+            return !string.IsNullOrEmpty(tableSchema)
+                ? startQuotationMark + tableSchema + endQuotationMark + "." + startQuotationMark + tableName + endQuotationMark
+                : startQuotationMark + tableName + endQuotationMark;
+        }
+
+        private string GetTableNameWithQuotes(JoinAttributeBase attrJoin, SqlPropertyMetadata[] props, string tableName)
+        {
+            switch (Provider)
+            {
+                case SqlProvider.MSSQL:
+                    tableName = "[" + tableName + "]";
+                    attrJoin.TableName = GetTableNameWithSchemaPrefix(attrJoin.TableName, attrJoin.TableSchema, "[", "]");
+                    attrJoin.Key = "[" + attrJoin.Key + "]";
+                    attrJoin.ExternalKey = "[" + attrJoin.ExternalKey + "]";
+                    attrJoin.TableAlias = string.IsNullOrEmpty(attrJoin.TableAlias) ? string.Empty : "[" + attrJoin.TableAlias + "]";
+                    foreach (var prop in props)
+                    {
+                        prop.ColumnName = "[" + prop.CleanColumnName + "]";
+                    }
+
+                    break;
+
+                case SqlProvider.MySQL:
+                    tableName = "`" + tableName + "`";
+                    attrJoin.TableName = GetTableNameWithSchemaPrefix(attrJoin.TableName, attrJoin.TableSchema, "`", "`");
+                    attrJoin.Key = "`" + attrJoin.Key + "`";
+                    attrJoin.ExternalKey = "`" + attrJoin.ExternalKey + "`";
+                    attrJoin.TableAlias = string.IsNullOrEmpty(attrJoin.TableAlias) ? string.Empty : "`" + attrJoin.TableAlias + "`";
+                    foreach (var prop in props)
+                    {
+                        prop.ColumnName = "`" + prop.CleanColumnName + "`";
+                    }
+
+                    break;
+
+                case SqlProvider.SQLite:
+                    break;
+
+                case SqlProvider.PostgreSQL:
+                    tableName = "\"" + tableName + "\"";
+                    attrJoin.TableName = GetTableNameWithSchemaPrefix(attrJoin.TableName, attrJoin.TableSchema, "\"", "\"");
+                    attrJoin.Key = "\"" + attrJoin.Key + "\"";
+                    attrJoin.ExternalKey = "\"" + attrJoin.ExternalKey + "\"";
+                    attrJoin.TableAlias = string.IsNullOrEmpty(attrJoin.TableAlias) ? string.Empty : "\"" + attrJoin.TableAlias + "\"";
+                    foreach (var prop in props)
+                    {
+                        prop.ColumnName = "\"" + prop.CleanColumnName + "\"";
+                    }
+
+                    break;
+
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(Provider));
+            }
+
+            return tableName;
+        }
+    }
+}
--- src/MicroOrm.Dapper.Repositories/SqlGenerator/SqlGenerator.cs
+++ src/MicroOrm.Dapper.Repositories/SqlGenerator/SqlGenerator.cs
@@ -134,66 +134,6 @@ namespace MicroOrm.Dapper.Repositories.SqlGenerator
             return joinPropertyMetadatas.ToArray();
         }
 
-        private static string GetTableNameWithSchemaPrefix(string tableName, string tableSchema, string startQuotationMark = "", string endQuotationMark = "")
-        {
-            return !string.IsNullOrEmpty(tableSchema)
-                ? startQuotationMark + tableSchema + endQuotationMark + "." + startQuotationMark + tableName + endQuotationMark
-                : startQuotationMark + tableName + endQuotationMark;
-        }
-
-        private string GetTableNameWithQuotes(JoinAttributeBase attrJoin, SqlPropertyMetadata[] props, string tableName)
-        {
-            switch (Provider)
-            {
-                case SqlProvider.MSSQL:
-                    tableName = "[" + tableName + "]";
-                    attrJoin.TableName = GetTableNameWithSchemaPrefix(attrJoin.TableName, attrJoin.TableSchema, "[", "]");
-                    attrJoin.Key = "[" + attrJoin.Key + "]";
-                    attrJoin.ExternalKey = "[" + attrJoin.ExternalKey + "]";
-                    attrJoin.TableAlias = string.IsNullOrEmpty(attrJoin.TableAlias) ? string.Empty : "[" + attrJoin.TableAlias + "]";
-                    foreach (var prop in props)
-                    {
-                        prop.ColumnName = "[" + prop.CleanColumnName + "]";
-                    }
-
-                    break;
-
-                case SqlProvider.MySQL:
-                    tableName = "`" + tableName + "`";
-                    attrJoin.TableName = GetTableNameWithSchemaPrefix(attrJoin.TableName, attrJoin.TableSchema, "`", "`");
-                    attrJoin.Key = "`" + attrJoin.Key + "`";
-                    attrJoin.ExternalKey = "`" + attrJoin.ExternalKey + "`";
-                    attrJoin.TableAlias = string.IsNullOrEmpty(attrJoin.TableAlias) ? string.Empty : "`" + attrJoin.TableAlias + "`";
-                    foreach (var prop in props)
-                    {
-                        prop.ColumnName = "`" + prop.CleanColumnName + "`";
-                    }
-
-                    break;
-
-                case SqlProvider.SQLite:
-                    break;
-
-                case SqlProvider.PostgreSQL:
-                    tableName = "\"" + tableName + "\"";
-                    attrJoin.TableName = GetTableNameWithSchemaPrefix(attrJoin.TableName, attrJoin.TableSchema, "\"", "\"");
-                    attrJoin.Key = "\"" + attrJoin.Key + "\"";
-                    attrJoin.ExternalKey = "\"" + attrJoin.ExternalKey + "\"";
-                    attrJoin.TableAlias = string.IsNullOrEmpty(attrJoin.TableAlias) ? string.Empty : "\"" + attrJoin.TableAlias + "\"";
-                    foreach (var prop in props)
-                    {
-                        prop.ColumnName = "\"" + prop.CleanColumnName + "\"";
-                    }
-
-                    break;
-
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(Provider));
-            }
-
-            return tableName;
-        }
-
         private string AppendJoinToUpdate<TBase>(TBase entity, SqlQuery originalBuilder, params Expression<Func<TEntity, object>>[] includes)
         {
             var joinBuilder = new StringBuilder();
