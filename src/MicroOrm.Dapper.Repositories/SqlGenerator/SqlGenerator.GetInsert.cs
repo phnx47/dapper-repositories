41da4ab573385d05b5dea7de723830118c4102be
Serge K <cmt@phnx47.net>
2022-01-29 15:03:17 +0700

    refactor: extract SqlGenerator.GetInsert

---
+++ src/MicroOrm.Dapper.Repositories/SqlGenerator/SqlGenerator.GetInsert.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using MicroOrm.Dapper.Repositories.Attributes;
+
+namespace MicroOrm.Dapper.Repositories.SqlGenerator
+{
+    /// <inheritdoc />
+    public partial class SqlGenerator<TEntity>
+        where TEntity : class
+    {
+        /// <inheritdoc />
+        public virtual SqlQuery GetInsert(TEntity entity)
+        {
+            var properties =
+                (IsIdentity
+                    ? SqlProperties.Where(p => !p.PropertyName.Equals(IdentitySqlProperty.PropertyName, StringComparison.OrdinalIgnoreCase))
+                    : SqlProperties).ToList();
+
+            if (HasUpdatedAt)
+            {
+                var attribute = UpdatedAtProperty.GetCustomAttribute<UpdatedAtAttribute>();
+                var offset = attribute.TimeKind == DateTimeKind.Local
+                    ? new DateTimeOffset(DateTime.Now)
+                    : new DateTimeOffset(DateTime.UtcNow);
+                if (attribute.OffSet != 0)
+                {
+                    offset = offset.ToOffset(TimeSpan.FromHours(attribute.OffSet));
+                }
+
+                UpdatedAtProperty.SetValue(entity, offset.DateTime);
+            }
+
+            var query = new SqlQuery(entity);
+
+            query.SqlBuilder.AppendFormat("INSERT INTO {0} ({1}) VALUES ({2})", TableName, string.Join(", ", properties.Select(p => p.ColumnName)),
+                string.Join(", ", properties.Select(p => "@" + p.PropertyName))); // values
+
+            if (IsIdentity)
+                switch (Provider)
+                {
+                    case SqlProvider.MSSQL:
+                        query.SqlBuilder.Append(" SELECT SCOPE_IDENTITY() AS " + IdentitySqlProperty.ColumnName);
+                        break;
+
+                    case SqlProvider.MySQL:
+                        query.SqlBuilder.Append("; SELECT CONVERT(LAST_INSERT_ID(), SIGNED INTEGER) AS " + IdentitySqlProperty.ColumnName);
+                        break;
+
+                    case SqlProvider.SQLite:
+                        query.SqlBuilder.Append("; SELECT LAST_INSERT_ROWID() AS " + IdentitySqlProperty.ColumnName);
+                        break;
+
+                    case SqlProvider.PostgreSQL:
+                        query.SqlBuilder.Append(" RETURNING " + IdentitySqlProperty.ColumnName);
+                        break;
+
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+
+            return query;
+        }
+    }
+}
