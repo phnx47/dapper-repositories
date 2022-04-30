a89a858c196d154937dc8b1fba3be539ee7dca10
Serge K <cmt@phnx47.net>
2022-04-30 21:33:20 +0700

    test: update mssql connString

--- test/MicroOrm.Dapper.Repositories.Tests/DatabaseFixture/MsSql2019DatabaseFixture.cs
+++ test/MicroOrm.Dapper.Repositories.Tests/DatabaseFixture/MsSql2019DatabaseFixture.cs
@@ -3,7 +3,7 @@ namespace MicroOrm.Dapper.Repositories.Tests.DatabaseFixture
     public class MsSql2019DatabaseFixture : MsSqlDatabaseFixture
     {
         public MsSql2019DatabaseFixture()
-            : base("Server=localhost;Database=master;User ID=sa;Password=Password12!")
+            : base("Server=localhost;Database=master;User ID=sa;Password=Password12!;Trust Server Certificate=true")
         {
         }
     }
