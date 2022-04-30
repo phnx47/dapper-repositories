7a717f01af5656ec1ccda94af48fe4c5d30d7f2f
Serge K <cmt@phnx47.net>
2022-04-30 19:17:55 +0700

    chore: remove pack.ps1

--- pack.ps1
+++
@@ -1,7 +0,0 @@
-if ($env:APPVEYOR_REPO_TAG -eq "false") {
-  dotnet pack $env:APPVEYOR_BUILD_FOLDER\src\MicroOrm.Dapper.Repositories -c Release  --no-build --version-suffix build$env:APPVEYOR_BUILD_NUMBER -o artifacts\github
-}
-
-if ($env:APPVEYOR_REPO_TAG -eq "true") {
-  dotnet pack $env:APPVEYOR_BUILD_FOLDER\src\MicroOrm.Dapper.Repositories -c Release --no-build -o artifacts\nuget
-}
