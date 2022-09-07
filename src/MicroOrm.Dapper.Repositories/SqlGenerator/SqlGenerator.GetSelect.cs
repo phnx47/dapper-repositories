93f67549437f0b76df98e278316b0b67f4bfc9b7
彭伟 <pengweiqhca@sina.com>
2022-09-07 10:36:16 +0800

    Add virtual to GetSelectById

--- src/MicroOrm.Dapper.Repositories/SqlGenerator/SqlGenerator.GetSelect.cs
+++ src/MicroOrm.Dapper.Repositories/SqlGenerator/SqlGenerator.GetSelect.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using MicroOrm.Dapper.Repositories.SqlGenerator.Filters;
 
 namespace MicroOrm.Dapper.Repositories.SqlGenerator
 {
