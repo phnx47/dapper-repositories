d446b30c115b2026efed5d4562b7c6dccbc50257
Serge K <cmt@phnx47.net>
2025-11-01 22:07:15 +0700

    IDE0290: Use primary constructor

--- tests/Repositories.Base/TestDbContext.cs
+++ tests/Repositories.Base/TestDbContext.cs
@@ -6,7 +6,7 @@ using TestClasses;
 
 namespace Repositories.Base;
 
-public class TestDbContext : DapperDbContext
+public class TestDbContext(IDbConnection connection, SqlProvider provider, bool useQuotationMarks = false) : DapperDbContext(connection)
 {
     private IDapperRepository<Address> _address;
 
@@ -20,25 +20,15 @@ public class TestDbContext : DapperDbContext
 
     private IDapperRepository<Phone> _phones;
 
-    private readonly SqlProvider _provider;
-    private readonly bool _useQuotationMarks;
+    public IDapperRepository<Address> Address => _address ??= new DapperRepository<Address>(Connection, new SqlGenerator<Address>(provider, useQuotationMarks));
 
-    public TestDbContext(IDbConnection connection, SqlProvider provider, bool useQuotationMarks = false)
-        : base(connection)
-    {
-        _provider = provider;
-        _useQuotationMarks = useQuotationMarks;
-    }
+    public IDapperRepository<User> Users => _users ??= new DapperRepository<User>(Connection, new SqlGenerator<User>(provider, useQuotationMarks));
 
-    public IDapperRepository<Address> Address => _address ??= new DapperRepository<Address>(Connection, new SqlGenerator<Address>(_provider, _useQuotationMarks));
+    public IDapperRepository<Car> Cars => _cars ??= new DapperRepository<Car>(Connection, new SqlGenerator<Car>(provider, useQuotationMarks));
 
-    public IDapperRepository<User> Users => _users ??= new DapperRepository<User>(Connection, new SqlGenerator<User>(_provider, _useQuotationMarks));
+    public IDapperRepository<City> Cities => _cities ??= new DapperRepository<City>(Connection, new SqlGenerator<City>(provider, useQuotationMarks));
 
-    public IDapperRepository<Car> Cars => _cars ??= new DapperRepository<Car>(Connection, new SqlGenerator<Car>(_provider, _useQuotationMarks));
+    public IDapperRepository<Report> Reports => _reports ??= new DapperRepository<Report>(Connection, new SqlGenerator<Report>(provider, useQuotationMarks));
 
-    public IDapperRepository<City> Cities => _cities ??= new DapperRepository<City>(Connection, new SqlGenerator<City>(_provider, _useQuotationMarks));
-
-    public IDapperRepository<Report> Reports => _reports ??= new DapperRepository<Report>(Connection, new SqlGenerator<Report>(_provider, _useQuotationMarks));
-
-    public IDapperRepository<Phone> Phones => _phones ??= new DapperRepository<Phone>(Connection, new SqlGenerator<Phone>(_provider, _useQuotationMarks));
+    public IDapperRepository<Phone> Phones => _phones ??= new DapperRepository<Phone>(Connection, new SqlGenerator<Phone>(provider, useQuotationMarks));
 }
